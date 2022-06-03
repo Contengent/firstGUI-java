492bf82602702acd90c9413b377f743b84ed439b
Contengent <103345406+Contengent@users.noreply.github.com>
2022-06-03 00:22:04 -0400

    Add files via upload

---
+++ firstGUI/src/main/java/com/mycompany/firstgui/FirstGUI.java
@@ -0,0 +1,17 @@
+package com.mycompany.firstgui;
+
+/**
+ * @author 
+ *
+ *   /\_/\  _     ___
+ *  |>'v'<||"| -/\, ,`>
+ *     -=Edelstein=-
+ * 
+ */
+
+public class FirstGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
